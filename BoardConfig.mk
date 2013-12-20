66587a0b1fe467a3cff8695b95ba561252557055
inyourface09 <getechdirect@gmail.com>
2013-12-20 15:21:31 -0600

    initial cwm commit

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,94 +1,29 @@
-# config.mk
-#
-# Product-specific compile-time definitions.
-#
+USE_CAMERA_STUB := true
 
-# The generic product target doesn't have any hardware-specific pieces.
-TARGET_NO_BOOTLOADER := true
-TARGET_NO_KERNEL := true
-
-PRODUCT_CHARACTERISTICS := tablet
+# inherit from the proprietary version
+-include vendor/rockchip/rk2928sdk/BoardConfigVendor.mk
 
 TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_ARCH_VARIANT_CPU := cortex-a8
-ARCH_ARM_HAVE_NEON := true
+TARGET_NO_BOOTLOADER := true
+TARGET_BOARD_PLATFORM := unknown
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_SMP := true
+TARGET_ARCH_VARIANT := armv7-a-neon
 ARCH_ARM_HAVE_TLS_REGISTER := true
-TARGET_GLOBAL_CFLAGS += -mtune=cortex-a8 -mfpu=neon
-# -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a8 -mfpu=neon
-# -mfloat-abi=softfp
-TARGET_BOARD_PLATFORM := rk29board
-TARGET_BOARD_HARDWARE := rk29board
 
-# Use a smaller subset of system fonts to keep image size lower
-SMALLER_FONT_FOOTPRINT := true
-#MINIMAL_FONT_FOOTPRINT := true
+TARGET_BOOTLOADER_BOARD_NAME := rk2928sdk
 
-TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/kernel
+BOARD_KERNEL_CMDLINE := 
+BOARD_KERNEL_BASE := 0x60400000
+BOARD_KERNEL_PAGESIZE := 16384
 
-# Init
-TARGET_PROVIDES_INIT := true
-TARGET_PROVIDES_INIT_TARGET_RC := true
-TARGET_RECOVERY_INITRC := $(LOCAL_PATH)/recovery/init.rc
+# fix this up by examining /proc/mtd on a running device
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x105c0000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x105c0000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x105c0000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
+BOARD_FLASH_BLOCK_SIZE := 131072
 
-# Recovery
-BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/rockchip/rk29board/recovery/recovery_keys.c
-BOARD_UMS_2ND_LUNFILE := "/sys/class/android_usb/android0/f_mass_storage/lun1/file"
-BOARD_UMS_LUNFILE := "/sys/class/android_usb/android0/f_mass_storage/lun0/file"
+TARGET_PREBUILT_KERNEL := device/rockchip/rk2928sdk/kernel
+
 BOARD_HAS_NO_SELECT_BUTTON := true
-BOARD_HAS_LARGE_FILESYSTEM := true
-
-
-# Some framework code requires this to enable BT
-BOARD_HAVE_BLUETOOTH := false
-
-# WLAN
-BOARD_WPA_SUPPLICANT_DRIVER := WEXT
-WPA_SUPPLICANT_VERSION      := VER_0_8_X
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
-BOARD_HOSTAPD_DRIVER        := WEXT
-BOARD_HOSTAPD_PRIVATE_LIB   := lib_driver_cmd_bcmdhd
-BOARD_WLAN_DEVICE           := bcmdhd
-WIFI_DRIVER_MODULE_PATH     := "/system/lib/modules/wlan.ko"
-WIFI_DRIVER_MODULE_NAME     := "wlan"
-
-# Graphics
-BOARD_EGL_CFG := device/rockchip/rk29board/egl.cfg
-USE_OPENGL_RENDERER := true
-BOARD_USE_LEGACY_UI := true
-
-# Audio
-#BOARD_USES_GENERIC_AUDIO := true
-#BOARD_USES_ALSA_AUDIO := true
-
-# Camera Setup
-#BOARD_USES_LEGACY_CAMERA := true
-USE_CAMERA_STUB := true
-#BOARD_CAMERA_USE_MM_HEAP := true
-
-# No HWCOMPOSER - TO DO
-BOARD_USES_HWCOMPOSER := true
-
-# Misc display settings
-BOARD_USE_SKIA_LCDTEXT := true
-BOARD_HAVE_VPU := true
-
-# Enable WEBGL in WebKit
-ENABLE_WEBGL := true
-TARGET_FORCE_CPU_UPLOAD := true
-
-# Sensor
-#BOARD_SENSOR_ST := true
-#BOARD_SENSOR_NORMAL := true
-#BOARD_SENSOR_MID := true   #error
-#BOARD_SENSOR_MPU := true
-
-# Vold
-BOARD_VOLD_MAX_PARTITIONS := 12
-BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
-BOARD_VOLD_DISC_HAS_MULTIPLE_MAJORS := true
-TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/f_mass_storage/lun%d/file"
--- cm.mk
+++ cm.mk
@@ -1,20 +1,18 @@
 ## Specify phone tech before including full_phone
-#$(call inherit-product, vendor/cm/config/gsm.mk)
-
-# Inherit device configuration
-$(call inherit-product, device/rockchip/rk29board/device.mk)
-
-# Inherit some common CM stuff. 
-# Changed to not get the proprietary stuff
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Release name
-PRODUCT_RELEASE_NAME := rk29board
+PRODUCT_RELEASE_NAME := rk2928sdk
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/rockchip/rk2928sdk/device_rk2928sdk.mk)
 
 ## Device identifier. This must come after all inclusions
-# Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := cm_rk29board
-PRODUCT_DEVICE := rk29board
-PRODUCT_BRAND := Android
-PRODUCT_MODEL := CM on rk29board
-PRODUCT_MANUFACTURER := Rockchip
+PRODUCT_DEVICE := rk2928sdk
+PRODUCT_NAME := cm_rk2928sdk
+PRODUCT_BRAND := rockchip
+PRODUCT_MODEL := rk2928sdk
+PRODUCT_MANUFACTURER := rockchip
