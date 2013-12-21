08af34a970bfb2c5a7176aa235f24c6e9ed0eb5b
inyourface09 <getechdirect@gmail.com>
2013-12-20 19:23:06 -0600

    add ramdisk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,7 +9,10 @@ TARGET_BOARD_PLATFORM := unknown
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_VARIANT := cortex-a9
 ARCH_ARM_HAVE_TLS_REGISTER := true
+TARGET_BOARD_PLATFORM := rk29board
+TARGET_BOARD_HARDWARE := rk29board
 
 TARGET_BOOTLOADER_BOARD_NAME := rk2928sdk
 
@@ -26,4 +29,14 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 
 TARGET_PREBUILT_KERNEL := device/rockchip/rk2928sdk/kernel
 
+# Init
+TARGET_PROVIDES_INIT := true
+TARGET_PROVIDES_INIT_TARGET_RC := true
+TARGET_RECOVERY_INITRC := $(LOCAL_PATH)/recovery/init.rc
+
+# Recovery
+BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/rockchip/rk2928sdk/recovery/recovery_keys.c
+BOARD_UMS_2ND_LUNFILE := "/sys/class/android_usb/android0/f_mass_storage/lun1/file"
+BOARD_UMS_LUNFILE := "/sys/class/android_usb/android0/f_mass_storage/lun0/file"
 BOARD_HAS_NO_SELECT_BUTTON := true
+PRODUCT_CHARACTERISTICS := tablet
